171fef73a82defcd208d017f7023ced70ed9258d
Muhammad Anas <298232247+MuhammadANAS-dev5@users.noreply.github.com>
2026-07-16 20:44:55 +0500

    upload OOP files.

---
+++ 08_1_Composition.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#define size 10
+
+class Element {
+    public:
+    int value;
+    Element() {
+        value = 0;
+        cout << "Element constructed!: " <<value<< endl; 
+    }    
+};
+
+class Collection 
+{
+    public:
+    Element el1, el2;
+    Collection() { 
+        cout<<"Collection constructed!" << endl; 
+    }
+};
+
+int main(void) 
+{
+    Collection coll;
+    cout<<"Size of Collection: "<<sizeof(coll)<<endl;
+    cout<<coll.el1.value<<endl;
+    cout<<coll.el2.value;
+    return 0;
+}
